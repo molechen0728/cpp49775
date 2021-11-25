cf23b4f683b85758cbf7b9212e8d0df183efb9c6
chenmo <chenmo@speakin.ai>
2021-11-25 13:45:18 +0800

    <init>

---
+++ cpp49775.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "cpp49775.h"
+#include <stdlib.h>
+
+
+SomeData __stdcall new_data()
+{
+	SomeData sd;
+	sd.data = (char*)malloc(sizeof(char) * 1024);
+	sd.len = 1024;
+	return sd;
+}
+
+void __stdcall delete_data(SomeData* data)
+{
+	free(data->data);
+	data->len = 0;
+}
---
+++ cpp49775.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+typedef struct {
+    char* data;
+    unsigned int len;
+} SomeData;
+
+extern "C" _declspec(dllexport) SomeData __stdcall new_data();
+extern "C" _declspec(dllexport) void __stdcall delete_data(SomeData * data);
